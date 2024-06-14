8ebb2268e5689c7e7db3b7c18aec8805af4f9ef5
Sentakuu <Sentakuu@sentakuu>
2024-06-14 17:15:47 +0200

    Test

---
+++ src/main/java/net/mcreator/aetheris/ClientProxyAetheris.java
@@ -0,0 +1,26 @@
+package net.mcreator.aetheris;
+
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.client.model.obj.OBJLoader;
+
+public class ClientProxyAetheris implements IProxyAetheris {
+	@Override
+	public void init(FMLInitializationEvent event) {
+	}
+
+	@Override
+	public void preInit(FMLPreInitializationEvent event) {
+		OBJLoader.INSTANCE.addDomain("aetheris");
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event) {
+	}
+
+	@Override
+	public void serverLoad(FMLServerStartingEvent event) {
+	}
+}
